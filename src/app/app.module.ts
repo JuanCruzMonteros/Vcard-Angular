0a0f9a71673dbe922850c01ea597bbf4e50769cc
JuanCruzMonteros <jjuancruzrey@gmail.com>
2019-01-25 22:30:29 -0300

    inicializando componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AboutComponent } from './menu/about/about.component';
+import { BlogComponent } from './menu/blog/blog.component';
+import { ContactoComponent } from './menu/contacto/contacto.component';
+import { PortafolioComponent } from './menu/portafolio/portafolio.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    BlogComponent,
+    ContactoComponent,
+    PortafolioComponent
   ],
   imports: [
     BrowserModule
